aa6b3edcd7cf615c711825d0d1885f36901430e6
Dylan Fischler <dylanfischler@gmail.com>
2016-12-06 22:59:12 -0500

    beauty

--- app/public/src/js/components/start/index.js
+++ app/public/src/js/components/start/index.js
@@ -13,24 +13,31 @@ class Start extends React.Component {
         });
     }
 
+    componentDidMount() {
+        setTimeout(() => this.setState({ visible: true }), 0);
+    }
+
     render() {
         return (
-			<div className="container center-align">
+			<div id="start" className={`container center-align animate fade ${this.state.visible ? 'in':''}`}>
                 <div>
                     <h3>Welcome to Cerebro<span className="sub">, the mutation visualization tool.</span></h3>
                 </div>
                 <div id="dir-chooser" className="container">
-                    <p>Choose the directory with your mutation analysis:</p>
-                    <div>
-                        <button onClick={() => this.openDialog()}>Open</button>
-                    </div>
-                </div>
-                <div style={{marginTop: '30px'}}>
-                    <button 
-                        onClick={() => this.props.onChooseDirectory(this.state.dir)}
-                        disabled={!this.state.dir}>
-                        Visualize
-                    </button>
+                    <ul>
+                        <li>
+                            <button className="primary" onClick={() => this.openDialog()}>Choose Directory</button>
+                        </li>
+                        <li>then...</li>
+                        <li>
+                            <button 
+                                className="primary"
+                                onClick={() => this.props.onChooseDirectory(this.state.dir)}
+                                disabled={!this.state.dir}>
+                                Visualize
+                            </button>
+                        </li>
+                    </ul>
                 </div>
 			</div>
 		);
