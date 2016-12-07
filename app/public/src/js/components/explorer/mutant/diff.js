887ea9d784d369160240caa6f57d4889662b307e
Cameron Wheeler <cameron@scelos.com>
2016-12-07 13:54:03 -0500

    Diff views

---
+++ app/public/src/js/components/explorer/mutant/diff.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Diff extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+		    <div className='diff'>
+                Diff View
+            </div>
+		);
+    }
+}
+
+export default Diff;
--- app/public/src/js/components/explorer/mutant/index.js
+++ app/public/src/js/components/explorer/mutant/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Diff from './diff';
 
 class Mutant extends React.Component {
     constructor(props) {
         super(props);
 
         this.toggle = this.toggle.bind(this);
+        this.expandedHeight = this.expandedHeight.bind(this);
     }
 
     componentWillMount() {
@@ -28,12 +30,30 @@ class Mutant extends React.Component {
         window.document.dispatchEvent(new Event('contractMutants'));
     }
 
+    expandedHeight() {
+        return 18 + this.expandContent.clientHeight;
+    }
+
     render() {
         return (
-			<li onClick={this.toggle} className={this.state.expanded && 'expanded'}>
+			<li onClick={this.toggle} className={this.state.expanded && 'expanded'} style={this.state.expanded ? {height: this.expandedHeight()} : null}>
                 <div className='title'>
                     <div className={this.props.killed ? 'indicator-killed' : 'indicator-live'}></div>
                     Mutant {this.props.id}
+                </div>
+
+                <div className='expand-content' ref={elem => this.expandContent = elem}>
+                    <div className='location'>HashMap:35</div>
+
+                    <div className='column-layout diffs'>
+                        <div className='column-1-right'>
+                            <Diff />
+                        </div>
+
+                        <div className='column-1-left'>
+                            <Diff />
+                        </div>
+                    </div>
                 </div>
 			</li>
 		);
